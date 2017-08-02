54b51d7386c8f84d76a8bdd377e4d0f5ee86b4cf
Pesky12 <Pesky12@users.noreply.github.com>
2017-08-02 20:09:32 +0200

    Help is now in embed.

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,14 @@
+const Discord = require('discord.js')
 const settings = require('../config.json');
 exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`<List of commands for Gbot>\n\n{Use ${settings.prefix}help <command> for more info!}\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+    const embed = new Discord.RichEmbed()
+      .setAuthor(`List of commands for Gbot`)
+      .setDescription(`\`Use ${settings.prefix}help <command> for more info!}\n\n\` \`\`\`${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} || ${c.help.description}`).join('\n')}\`\`\``)
+      .setColor('#17b3d6')
+    message.channel.send({embed});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
