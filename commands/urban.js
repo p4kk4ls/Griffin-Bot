7ed1708f3767f3c9028e1cf6d552f847266dba31
Pesky12 <Pesky12@users.noreply.github.com>
2017-08-02 20:09:44 +0200

    Urban command!!

---
+++ commands/urban.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const urban = require('relevant-urban');
+exports.run = (client, message, args) =>{
+  if(!args[0]){
+    urban.random().then(urbaned =>{
+      const embed = new Discord.RichEmbed()
+        .setAuthor(`Random urban of "${urbaned.word}"`)
+        .setDescription(urbaned.definition)
+        .setColor('#17b3d6')
+        .addField('Example', urbaned.example)
+        .setFooter(`${urbaned.urbanURL} | Likes: ${urbaned.thumbsUp} | Dislikes: ${urbaned.thumbsDown}`);
+      message.channel.send({embed});
+    });
+    return
+  }
+
+  let toUrban = args.join(' '); 
+  urban(toUrban).then(urbaned =>{
+    const embed = new Discord.RichEmbed()
+      .setAuthor(`Urban of "${urbaned.word}"`)
+      .setDescription(urbaned.definition)
+      .setColor('#17b3d6')
+      .addField('Example', urbaned.example)
+      .addField('Tags', urbaned.tags.join(' '))
+      .setFooter(`${urbaned.urbanURL} | Likes: ${urbaned.thumbsUp} | Dislikes: ${urbaned.thumbsDown}`);
+    message.channel.send({embed});
+  });
+};
+
+
+exports.help = {
+  name: 'urban',
+  description: '🔧 Searches for a word/sentence on "Urban Dictionary"',
+  usage: 'urban [word | sentence]'
+};
