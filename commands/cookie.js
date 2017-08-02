dd411a9f7e3338326ab86be4299b753116a906a6
Pesky12 <Pesky12@users.noreply.github.com>
2017-08-02 20:58:29 +0200

    now its fully sandvich

--- commands/cookie.js
+++ commands/cookie.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) =>{
 
   if (message.mentions.users.size < 1) { 
     let embed = new Discord.RichEmbed()
-      .setTitle('You havent said who\'s cookie it is so i ate it.')
+      .setTitle('You havent said who\'s sandvich it is so i ate it.')
       .setColor('#683e0d')
       .setThumbnail('http://www.markrice.com/birds/2005images/20051211_0031%20PiperHubble%20Eating%20Sandwich.JPG');
 
@@ -15,7 +15,7 @@ exports.run = (client, message, args) =>{
 
   let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL)
-    .setTitle(`Sent you a cookie ${giveCookieto.username}!`)
+    .setTitle(`Sent you a sandvich ${giveCookieto.username}!`)
     .setColor('#683e0d')
     .setThumbnail('https://wiki.teamfortress.com/w/images/thumb/9/95/Sandvich.png');
   message.channel.send({embed});
