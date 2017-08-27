36b512a21d236481ad94a8b2d86891fceda62be2
peskyn12 <alesoelsov@gmail.com>
2017-08-27 22:59:12 +0200

    Finnished

---
+++ PerServer/UGS/assign.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+let guildID = '349630476496928769';
+
+module.exports = (client) => {
+  client.on('message', message => {
+    let messageAray = message.content.split(' | ');
+
+    // if (message.guild.id !== guildID) return;
+    if (message.content.startsWith('assign~')){
+      let title = messageAray[0].slice(6);
+      let description = messageAray[1];
+      let assignedUsers = message.mentions.users.array();
+      let assignedRole = message.mentions.roles.array()
+
+      if(!title) {
+        message.channel.send('Pls add args thx :ok_hand:');
+      }
+      let embedAssign = new Discord.RichEmbed()
+        .setAuthor(`${title} ᅠ`, 'https://cdn3.iconfinder.com/data/icons/illustricon-tech/512/task.board-512.png')  
+        .setDescription(`${description}ᅠ`)
+        .addField('Assigned Users', `${assignedUsers} ${assignedRole} ᅠ`)
+        .setColor('#0061ff')
+        .setFooter(`Assigned by: ${message.author.tag}ᅠ`)
+      // message.guild.channels.find('name', 'work-assign').send({embed: embedAssign});
+      // message.guild.channels.find('name', 'assign-history').send({embed: embedAssign});
+      message.channel.send({embed: embedAssign})
+      console.log('lol')
+
+    }
+
+  });
+};
--- Utils/require.js
+++ Utils/require.js
@@ -4,4 +4,5 @@ module.exports = (client) =>{
   require('../PerServer/Overwatch Blanca/main')(client);
   require('../PerServer/mahamas ART/main')(client);
   require('../PerServer/Pits Discord/main')(client);
+  require('../PerServer/UGS/main')(client);
 };
