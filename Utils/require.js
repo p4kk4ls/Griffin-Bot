f850a76534bea1b02e8d382ea261c78a72fd7ae0
peskyn12 <alesoelsov@gmail.com>
2017-08-27 17:51:12 +0200

    Requires no UGS

--- Utils/require.js
+++ Utils/require.js
@@ -4,5 +4,4 @@ module.exports = (client) =>{
   require('../PerServer/Overwatch Blanca/main')(client);
   require('../PerServer/mahamas ART/main')(client);
   require('../PerServer/Pits Discord/main')(client);
-  require('../PerServer/UGS/main')(client;)
 };
