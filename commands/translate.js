4ba05596acc1d48714dca0cc965b399feecac9df
peskyn12 <alesoelsov@gmail.com>
2017-09-08 20:24:21 +0200

    Initial commit

---
+++ commands/translate.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) =>{
+
+};
+
+exports.help = {
+  name: 'translate',
+  description: 'TODO',
+  usage: 'translate [Text to translate] | [to] | [from]'
+};
