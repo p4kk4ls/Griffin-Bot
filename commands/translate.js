192e71df64bb9e9358f330399f55a5756f31d0de
peskyn12 <alesoelsov@gmail.com>
2017-09-09 21:56:11 +0200

    initial commit (untested)

--- commands/translate.js
+++ commands/translate.js
@@ -1,7 +1,43 @@
 const Discord = require('discord.js');
+const translate = require('google-translate-api');
 
-exports.run = (client, message, args) =>{
+exports.run = (client, message) =>{
+  let messageAray = message.content.split(' | ');
 
+  let toTranslate = messageAray[0].slice(10);
+  let langTo = messageAray[1];
+  let langFrom = messageAray[2];
+
+  translate(toTranslate, {from: langFrom, to: langTo}).then(translated => {
+    console.log(translated.text);
+    // //=> Ik spreek Nederlands! 
+    // console.log(translated.from.text.autoCorrected);
+    // //=> true 
+    // console.log(translated.from.text.value);
+    // //=> I [speak] Dutch! 
+    // console.log(translated.from.text.didYouMean);
+    // //=> false 
+    console.log(translated);
+    if(translated.from.text.didYouMean || translated.from.text.autoCorrected){
+      var embed = new Discord.RichEmbed()
+        .setAuthor(`Translate: ${toTranslate}`, 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Translate-icon.png')
+        .addField('From:', translated.from.language.iso, true)
+        .addField('To:', langTo, true)
+        .addField('Did you mean?', translated.from.text.value)
+        .addField('Output', translated.text, true);
+    }
+
+    if(translated.from.text.autoCorrected){
+      var embed = new Discord.RichEmbed()
+        .setAuthor(`Translate: ${toTranslate}`, 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Translate-icon.png')
+        .addField('From:', translated.from.language.iso, true)
+        .addField('To:', langTo, true)
+        .addField('Output', translated.text, true);
+    }
+    message.channel.send({embed})
+  }).catch(err => {
+    console.error(err);
+  });
 };
 
 exports.help = {
@@ -9,3 +45,13 @@ exports.help = {
   description: 'TODO',
   usage: 'translate [Text to translate] | [to] | [from]'
 };
+
+// ┌─────────────────────
+// |Pesky12#8762 used '~translate Jek,se,mas,|,en,|,cs' in 'Gryphon L.O.G'/'beta-bot-only'
+// └─────────────────────
+// Jek se mas
+// { text: 'Jek se mas',
+//   from:
+//    { language: { didYouMean: true, iso: 'ms' },
+//      text: { autoCorrected: false, value: '[Jak] se mas', didYouMean: true } },
+//   raw: '' }
