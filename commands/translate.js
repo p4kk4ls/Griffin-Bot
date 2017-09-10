64f6ebc84d149fb83822a6e7fae1e24499bb9479
peskyn12 <alesoelsov@gmail.com>
2017-09-10 19:54:16 +0200

    Error warnings added

--- commands/translate.js
+++ commands/translate.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const translate = require('google-translate-api');
+const he = require('he');
 
 exports.run = (client, message) =>{
   let messageAray = message.content.split(' | ');
@@ -8,34 +9,53 @@ exports.run = (client, message) =>{
   let langTo = messageAray[1];
   let langFrom = messageAray[2];
 
+  if(!toTranslate){
+    let errorEmbed = new Discord.RichEmbed()
+      .setTitle('I have nothing to translate!')
+      .setColor('#ff0000');
+    message.channel.send({embed: errorEmbed}).then(botmsg =>{botmsg.delete(5000);});
+    return;
+  }
+  if(!langTo){
+    let errorEmbed = new Discord.RichEmbed()
+      .setTitle('Missing language argument.')
+      .setColor('#ff0000');
+    message.channel.send({embed: errorEmbed}).then(botmsg =>{botmsg.delete(5000);});
+    return;
+  }
+
   translate(toTranslate, {from: langFrom, to: langTo}).then(translated => {
-    console.log(translated.text);
-    // //=> Ik spreek Nederlands! 
-    // console.log(translated.from.text.autoCorrected);
-    // //=> true 
-    // console.log(translated.from.text.value);
-    // //=> I [speak] Dutch! 
-    // console.log(translated.from.text.didYouMean);
-    // //=> false 
-    console.log(translated);
+    var translatedText = he.decode(translated.text);
+    var translatedTextDidYouMean = he.decode(translated.from.text.value);
     if(translated.from.text.didYouMean || translated.from.text.autoCorrected){
       let embed = new Discord.RichEmbed()
         .setAuthor(`Translate: ${toTranslate}`, 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Translate-icon.png')
         .addField('From:', translated.from.language.iso, true)
         .addField('To:', langTo, true)
-        .addField('Did you mean?', translated.from.text.value)
-        .addField('Output', translated.text, true);
+        .addField('Did you mean?', translatedTextDidYouMean)
+        .setColor('#0273d6')
+        .addField('Output', translatedText);
+      message.channel.send({embed});
       return;
     }else{
       let embed = new Discord.RichEmbed()
         .setAuthor(`Translate: ${toTranslate}`, 'http://icons.iconarchive.com/icons/marcus-roberto/google-play/512/Google-Translate-icon.png')
         .addField('From:', translated.from.language.iso, true)
         .addField('To:', langTo, true)
-        .addField('Output', translated.text, true);
+        .addField('Output', translatedText)
+        .setColor('#0273d6');
       message.channel.send({embed});
       return;
     }
   }).catch(err => {
+    if(err.code == 400){
+      let errorEmbed = new Discord.RichEmbed()
+        .setTitle('This language is not supported!')
+        .setDescription('Try to use another formating (en | english)')
+        .setColor('#ff0000');
+      message.channel.send({embed: errorEmbed});
+      return;
+    }
     console.error(err);
     let errorEmbed = new Discord.RichEmbed()
       .setTitle('google-translate-api')
@@ -48,15 +68,5 @@ exports.run = (client, message) =>{
 exports.help = {
   name: 'translate',
   description: 'TODO',
-  usage: 'translate [Text to translate] | [to] | [from]'
+  usage: '🈸translate [Text to translate] | [to] | [from]'
 };
-
-// ┌─────────────────────
-// |Pesky12#8762 used '~translate Jek,se,mas,|,en,|,cs' in 'Gryphon L.O.G'/'beta-bot-only'
-// └─────────────────────
-// Jek se mas
-// { text: 'Jek se mas',
-//   from:
-//    { language: { didYouMean: true, iso: 'ms' },
-//      text: { autoCorrected: false, value: '[Jak] se mas', didYouMean: true } },
-//   raw: '' }
