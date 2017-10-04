62baeb4efb553af0df16f568cf4eba9cec2c69fb
peskyn12 <alesoelsov@gmail.com>
2017-10-04 10:35:14 +0200

    Added the thing

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,14 @@
-exports.run = (client, message) => {
-// if(message.author.)
-
-  message.channel.send('Ping?!')
-    .then(msg => {
-      msg.edit(`<:gun:333359555117580291> BANG! Ur dead! (Took me: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
-    });
+exports.run = (client, message, args,config) => {
+  if(message.author.id === config.ownerID && args){
+    let messageInput = args.slice(1);
+    let messageInputJs = messageInput.join(' ')
+    client.channels.get(args[0]).send(messageInputJs);
+  }else{
+    message.channel.send('Ping?!')
+      .then(msg => {
+        msg.edit(`<:gun:333359555117580291> BANG! Ur dead! (Took me: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
+      });
+  }
 };
 
 exports.help = {
