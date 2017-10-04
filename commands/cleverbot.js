88f0f4015dae35aa088eadf4db992608db6b5b2c
peskyn12 <alesoelsov@gmail.com>
2017-10-04 10:19:21 +0200

    CL clean up

--- commands/cleverbot.js
+++ commands/cleverbot.js
@@ -1,51 +1,17 @@
-const Discord = require('discord.js');
+const Cleverbot = require('cleverbot-node');
+const clbot = new Cleverbot;
 
-module.exports.run = async(client, message, args, config) => {
-    const Cleverbot = require('cleverbot-node');
-    const clbot = new Cleverbot;
+module.exports.run = (client, message, args, config) => {
+  clbot.configure({ botapi: config.clevertoken });
+  let Input = args.join();
 
-    clbot.configure({ botapi: config.clevertoken });
-
-    let Text = message.content.slice(4);
-
-    clbot.write(args, (response) => {
-        message.channel.startTyping();
-        setTimeout(() => {
-            message.channel.send(response.output).catch(console.error);
-            message.channel.stopTyping();
-            let embed = new Discord.RichEmbed()
-                .setAuthor(message.guild.name, message.guild.iconURL)
-                .addField(`${message.author.tag}:`, `${message.content}`)
-                .addField(`${message.guild.name}:`, `${response.output}`)
-                .setColor('#ff7700')
-                .setFooter('Logging', client.user.avatarURL)
-                .setTimestamp(new Date());
-            client.channels.get("331812235678580757").send({embed});
-        }, Math.random() * (1 - 3) + 1 * 1000);
-    });
-
-    //CLEVERIO TRY
-
-// const Cleverbotio = require('cleverbot.io');
-// const clbot = new Cleverbotio('uNMEYKBGQao1g7hK','CqYIqBSy284oQMufoTnYmcFkJMe5FY31');
-
-//     let input = message.content.slice(1)
-//     clbot.setNick(message.guild.name);
-//     message.channel.startTyping();
-//     clbot.create((err, session) => {
-//         clbot.ask(input, function (err, response) {
-//             message.channel.send(response).catch(console.error)
-//             message.channel.stopTyping();
-//               let embed = new Discord.RichEmbed()
-//                 .setAuthor(message.guild.name, message.guild.iconURL)
-//                 .addField(`${message.author.tag}:`, `${message.content}`)
-//                 .addField(`${message.guild.name}:`, `${response}`)
-//                 .setColor('#ff7700')
-//                 .setFooter('Logging', client.user.avatarURL)
-//                 .setTimestamp(new Date());
-//             client.channels.get("331812235678580757").send({embed});
-//         });
-//     });
+  clbot.write(Input, (response) => {
+    message.channel.startTyping();
+    setTimeout(() => {
+      message.channel.send(response.output).catch(console.error);
+      message.channel.stopTyping();
+    }, Math.random() * (1 - 3) + 1 * 1000);
+  });
 };
 
 exports.help = {
