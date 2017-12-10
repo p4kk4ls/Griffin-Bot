bb9e63b64f0c187a6e8013e65c97e0315257f311
peskyn12 <alesoelsov@gmail.com>
2017-12-10 20:10:51 +0100

    New loader in place

--- app.js
+++ app.js
@@ -29,87 +29,15 @@ switch(nodeArgs[0]) {
 
 const prefix = config.prefix;
 
-console.log(process.uptime())
 
 client.commands = new Discord.Collection
-client.commandsHidden = new Discord.Collection
 client.events = new Discord.Collection
-client.commandsDisabled = new Discord.Collection
 
 
-/**
- * Loads Everything in ./Events/
- */
-fs.readdir("./events", (err, files) =>{
-  if(err) return console.error(err)
-  let filesjs = files.filter(f => f.split(".").pop() === "js")
-  if(filesjs <= 0){
-      console.log('No Events found!!')
-      return
-  }
-
-  console.log(`\n┌──────────────────────────────────────┐\n|I am trying to load ${filesjs.length} events, hold up!`)
-  filesjs.forEach((f, i) => {
-    let file = require(`./events/${f}`)
-
-    if(file.settings.enabled == true){
-        console.log(`|${i + 1}: ${f} ready to fly!`)
-        file.run(client, config)
-        if(file.settings.public == true){
-            client.events.set(file.help.name, file);
-        }
-    } else {
-        console.log(`|${i + 1}: ${f} is disabled!`)
-    }
-  })
-  console.log('└──────────────────────────────────────┘')
-})
-
-fs.readdir("./Utils", (err, files) =>{
-    if(err) return console.error(err)
-    let filesjs = files.filter(f => f.split(".").pop() === "js")
-    if(filesjs <= 0){
-        console.log('No Utils found!!')
-        return
-    }
-  
-    console.log(`\n┌──────────────────────────────────────┐\n|I am trying to load ${filesjs.length} utils, hold up!`)
-    filesjs.forEach((f, i) => {
-        require(`./Utils/${f}`)(client, config)
-        console.log(`|${i + 1}: ${f} ready to fly!`)
-    })
-    console.log('└──────────────────────────────────────┘')
-  })
-
-/**
- * Commmand Collection
- */
-fs.readdir("./commands/", (err, files) =>{
-    if(err) return console.error(err)
-    let filesjs = files.filter(f => f.split(".").pop() === "js")
-    if(filesjs <= 0){
-        console.log('No commands to load mate!')
-        return
-    }
-
-    console.log(`\n┌──────────────────────────────────────┐\n|I am trying to load ${filesjs.length} commands, hold up!`)
-    filesjs.forEach((f, i) => {
-        let file = require(`./commands/${f}`)
-
-        if(file.settings.enabled == false) { 
-            client.commandsDisabled.set(file.help.name, file)
-            console.log(`|${i + 1}: ${f} is disabled!`)
-            return
-        }
-        console.log(`|${i + 1}: ${f} ready to fly!`)
-        if(file.settings.public == false){
-            client.commandsHidden.set(file.help.name, file);
-            return
-        }
-        client.commands.set(file.help.name, file);
-    })
-      console.log('└──────────────────────────────────────┘')
-})
+Loader('./commands/', client.commands)
+Loader('./events/', client.events, true)
+console.log(client.events)
+client.events.forEach((event) => { console.log("Running" + event.run); event.run() })
 
 /**
  * Command Handler
@@ -123,19 +51,40 @@ client.on('message', (message) =>{
   let command = messageAray[0];
   let args = messageAray.slice(1);
   let cmd = client.commands.get(command.slice(prefix.length))
-  let cmdHidden = client.commandsHidden.get(command.slice(prefix.length))
 
   if(cmd) {
-    if(cmd.settings.PM == false & message.channel.type !== 'text') return message.channel.send("This command is not allowed in PMs!!").then(message => message.delete(5000));
+    if(cmd.settings.PM == false & message.channel.type !== 'text') return message.channel.send("This command is not allowed in PMs!!")
     cmd.run(client, message, args, config)
-  }
-  if(cmdHidden){
-      if(message.author.id !== config.ownerID) return;
-    cmdHidden.run(client, message, args, config)
   }
 });
 
-if(testrun == true) return 0;
 
+function Loader(loadFolder, collection, requiring) {
+    fs.readdir(loadFolder, (err, files) => {
+        if (err)
+            return console.error(err);
+        let filesjs = files.filter(f => f.split(".").pop() === "js");
+        if (filesjs <= 0) {
+            console.log('No commands to load mate!');
+            return;
+        }
+        console.log(`\n──────────────────────────────────────\n>I am trying to load ${filesjs.length} files from ${loadFolder}, hold up!`);
+        filesjs.forEach((f, i) => {
+            let file = require(`${loadFolder}${f}`);
+            if (file.settings.enabled == false) {
+                console.log(`${i + 1}: ${f} is disabled and will not be loaded`);
+                return;
+            }
+            if(requiring == true){
+                file.run(client, config)
+            }
+            console.log(`${i + 1}: ${f} ready to fly!`);
+            collection.set(file.help.name, file);
+        });
+        console.log('──────────────────────────────────────');
+    });
+}
 
+if(testrun == true) process.exit(0);
 client.login(config.token);
+
