213aaa2534cee52cd6bd8922e7feb57cbd766d1d
peskyn12 <alesoelsov@gmail.com>
2017-12-11 11:48:53 +0100

    await

--- commands/roll.js
+++ commands/roll.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const randomgen = require('random-natural');
 
-exports.run = (client, message, args) =>{
-  var math = randomgen({ min: args[0], max: args[1] });
+exports.run = async(client, message, args) =>{
+  var math = await randomgen({ min: args[0], max: args[1] });
   let embed = new Discord.RichEmbed()
     .setTitle(`🎲 You rolled ${math}!`)
     .setColor('#2563c6');
