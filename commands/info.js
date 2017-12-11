a8018bd884f65733861b15f766a3a689cb3489bb
peskyn12 <alesoelsov@gmail.com>
2017-12-11 11:29:23 +0100

    Added links

--- commands/info.js
+++ commands/info.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
-exports.run = (client, message) =>{
+exports.run = async(client, message) =>{
+  let invitelink = await client.generateInvite()
   let embed = new Discord.RichEmbed()
     .setTitle(`Infoboard for ${client.user.username}`)
     .setDescription('Here is some info about this bot!!')
     .addField('Uptime', `${client.uptime} Miliseconds`,true)
     .addField('Code Creator', client.users.get('235047463017381888').tag, true)
+    .addField('Repo', 'https://bitbucket.org/Peskyn12/griffin-bot')
+    .addField('Invite me!!', invitelink)
     .setColor('#7f16ff')
     .setThumbnail(client.users.get('235047463017381888').avatarURL);
   message.channel.send({embed});
-  message.channel.send('https://bitbucket.org/Peskyn12/griffin-bot/');
 };
 
 exports.settings = {
