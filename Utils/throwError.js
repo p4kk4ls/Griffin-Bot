ec4692c5550aa1bd2f8e0ab36051be888af508fd
peskyn12 <alesoelsov@gmail.com>
2017-12-26 08:14:37 +0100

    More variants

--- Utils/throwError.js
+++ Utils/throwError.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js')
+const logHighPriority = '331072865707360258'
+const logLowPriority = '333727164937666562'
 
 
 /**
  * Sends an error embed with a description
  * @param {*Discord Channel} channel 
  * @param {*Error Title} errTitle 
- * @param {*Error Description} errDesc 
+ * @param {*Error Description} errDesc
+ * @param {*Colour} colour 
  */
-exports.throwEmbedDescription = (channel, errTitle, errDesc) => {
+exports.throwEmbedDescription = (channel, errTitle, errDesc, colour) => {
     channel.send({
         embed: new Discord.RichEmbed()
         .setTitle(errTitle)
-        .errDesc(errDesc)
+        .setDescription(errDesc)
+        .setColor(colour)
     })
 };
 
@@ -19,13 +23,43 @@ exports.throwEmbedDescription = (channel, errTitle, errDesc) => {
  * Sends an error emebed
  * @param {*Discord Channel} channel 
  * @param {*Error Title} errTitle 
+ * @param {*Colour} colour
  */
-exports.throwEmbed = (channel, errTitle) => {
+exports.throwEmbed = (channel, errTitle, colour) => {
     channel.send({
         embed: new Discord.RichEmbed()
         .setTitle(errTitle)
+        .setColor(colour)
     })
 };
+
+/**
+ * Sends a error to "High Priority Log"
+ * @param {*Discord Client} client 
+ * @param {*Discord Channel} channel 
+ * @param {*Error Title} errTitle 
+ * @param {*Error Desc} errDesc 
+ * @param {*Colour} colour 
+ */
+exports.throwErrorHighPriority = (client, channel, errTitle, errDesc, colour) => {
+    client.channels.get(logHighPriority).send({ 
+        embed: new Discord.RichEmbed()
+        .setTitle(errTitle)
+        .setDescription(errDesc)
+        .setColor(colour)
+        .setFooter(channel.name + '|' + channel.guild.name)
+    })
+}
+
+exports.throwErrorLowPriority = (client, channel, errTitle, errDesc, colour) => {
+    client.channels.get(logLowPriority).send({ 
+        embed: new Discord.RichEmbed()
+        .setTitle(errTitle)
+        .setDescription(errDesc)
+        .setColor(colour)
+        .setFooter(channel.name + '|' + channel.guild.name)
+    })
+}
 
 exports.settings = {
     enabled: false,     
