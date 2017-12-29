e2bb53bba85aa1da513010972b9d7893683858ff
peskyn12 <alesoelsov@gmail.com>
2017-12-29 23:32:00 +0100

    Now doge

--- commands/doge.js
+++ commands/doge.js
@@ -2,13 +2,14 @@ const got = require('got');
 const Discord = require('discord.js')
 
 exports.run = (client, message, args, config) => {
-  got(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${args[0]}&tsyms=USD`, { json: true }).then(response => {
+  got(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=DOGE&tsyms=USD`, { json: true }).then(response => {
     // 396416921030885379
     message.channel.send({ 
       embed: new Discord.RichEmbed()
-      .setAuthor(response.body['DISPLAY']['DOGE']['USD']['FROMSYMBOL'], 'hhttps://cdn2.iconfinder.com/data/icons/bitcoin-and-mining/44/trade-512.png')
-      .addField('Price (USD)', response.body['DISPLAY'][1][1]['PRICE'])
-      .addField('Today\'s high', response.body['DISPLAY'][1][1]['HIGHDAY'], true)
+      .setAuthor(response.body['DISPLAY']['DOGE']['USD']['FROMSYMBOL'], 'http://hoekomikaangeld.com/wp-content/uploads/2015/12/dogecoin-logo.png')
+      .setThumbnail('https://altcoins.com.au/img/logo-lg.png')
+      .addField('Price (USD)', response.body['DISPLAY']['DOGE']['USD']['PRICE'])
+      .addField('Today\'s high', response.body['DISPLAY']['DOGE']['USD']['HIGHDAY'], true)
       .addField('Today\s low', response.body['DISPLAY']['DOGE']['USD']['LOWDAY'], true)
       .setColor('#FFDF00')
       .setFooter('cryptocompare.com', 'https://freeter.io/embedding-web-apps/cryptocurrency/cryptocompare.png')
@@ -28,7 +29,7 @@ exports.settings = {
 };
 
 exports.help = {
-  name: 'crypto',
+  name: 'doge',
   description: '',
   longDescription: "",
   usage: ''
