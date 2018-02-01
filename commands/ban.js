7c9f1509216beb87858777f1f0f3e535deb05313
peskyn12 <alesoelsov@gmail.com>
2018-02-01 10:01:08 +0100

    Added "Bot Can't" messages

--- commands/ban.js
+++ commands/ban.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
   let reason = args.slice(usersToBan.array().length).join(' ') || 'There is none! ¯\\_(ツ)_/¯'
 
   usersToBan.map(u => {
-    if (!message.guild.members.find('id', u.id).bannable) return message.channel.send(randomMessages.botCant(u))
+    if (!message.guild.members.find('id', u.id).bannable) return message.channel.send(randomMessages.botCant(u, 'ban'))
     let em = modLogEmbed.modActionEmbed('Ban', message.author, u, reason)
     if (message.guild.channels.find('name', 'mod-log')) message.guild.channels.find('name', 'mod-log').send({embed: em})
     message.channel.send(randomMessages.ban(message.author, u))
