1f0ab43e7b2d484f3d3a92d820450a84a29969ec
peskyn12 <alesoelsov@gmail.com>
2018-02-01 10:01:54 +0100

    Added ban and botCant messages

--- utils/randomSelector.js
+++ utils/randomSelector.js
@@ -3,12 +3,23 @@
  * @param {*array} randomGreetings
  */
 exports.ban = (e, b) => {
-  let banMessages = [
-    `${b.username} was banned.`,
-    `${b.username} banned 2`
+  let messages = [
+    `${b} has been slashed by the hammer!`,
+    `${b}, hammer by-bye!`
 
   ]
-  return randomize(banMessages)
+  return randomize(messages)
+}
+
+exports.botCant = (u, a) => {
+  let messages = [
+    `${u} has more power then me! I can't ${a} him`,
+    `I can't ${a} ${u}, he has the high ground.`,
+    `${u} has a blessing. I can't ${a} him!`,
+    `I wish I could ${a} ${u}.`
+
+  ]
+  return randomize(messages)
 }
 
 function randomize (randomGreetings) {
